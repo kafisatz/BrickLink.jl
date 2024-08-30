ff7a318922a3d9cb6d53145e9a7eef0967796017
Bernhard König <github@diro.ch>
2024-08-30 17:39:12 +0200

    edt

--- src/prices.jl
+++ src/prices.jl
@@ -66,7 +66,7 @@ function get_prices(credentials,di::Dict,sets::DataFrames.DataFrame)
     #request prices from BrickLink API
     dfres = DataFrames.DataFrame()
     for setno in sets.set_no
-        @show setno
+        #@show setno
         setnostring = string(setno) * "-1"
         thisdi = deepcopy(di)
         thisdi["no"] = setnostring
